40d96a5e03ce154bee7c6bb9f861e3824e7fcbf8
Bohdan <bosrok.ke@gmail.com>
2024-09-22 15:04:16 +0100

    finish

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,62 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact, selectContacts } from '../../redux/contactsSlice';
+import { nanoid } from 'nanoid';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import styles from './ContactForm.module.css';
+
+const validationSchema = Yup.object({
+    name: Yup.string()
+        .min(3, 'Must be at least 3 characters')
+        .max(50, 'Must be 50 characters or less')
+        .required('Required'),
+    number: Yup.string()
+        .min(7, 'Must be at least 7 characters')
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+});
+
+const ContactForm = () => {
+    const dispatch = useDispatch();
+    const contacts = useSelector(selectContacts);
+
+    const handleSubmit = (values, { resetForm }) => {
+        const { name, number } = values;
+
+        const isDuplicate = contacts.some(
+            (contact) => contact.name.toLowerCase() === name.toLowerCase(),
+        );
+
+        if (isDuplicate) {
+            alert(`${name} is already in contacts.`);
+            return;
+        }
+
+        dispatch(addContact({ id: nanoid(), name, number }));
+        resetForm();
+    };
+
+    return (
+        <Formik
+            initialValues={{ name: '', number: '' }}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+            <Form className={styles.form}>
+                <label>
+                    Name
+                    <Field name="name" type="text" />
+                    <ErrorMessage name="name" component="div" />
+                </label>
+                <label>
+                    Number
+                    <Field name="number" type="text" />
+                    <ErrorMessage name="number" component="div" />
+                </label>
+                <button type="submit">Add contact</button>
+            </Form>
+        </Formik>
+    );
+};
+
+export default ContactForm;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+import { selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
+import Contact from '../contact/Contact';
+import styles from './ContactList.module.css';
+
+const ContactList = () => {
+    const contacts = useSelector(selectContacts);
+    const filter = useSelector(selectNameFilter);
+
+    const getVisibleContacts = () => {
+        const normalizedFilter = filter.toLowerCase();
+        return contacts.filter((contact) =>
+            contact.name.toLowerCase().includes(normalizedFilter),
+        );
+    };
+
+    const visibleContacts = getVisibleContacts();
+
+    return (
+        <ul className={styles.contactList}>
+            {visibleContacts.map(({ id, name, number }) => (
+                <Contact key={id} id={id} name={name} number={number} />
+            ))}
+        </ul>
+    );
+};
+
+export default ContactList;
